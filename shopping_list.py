b65769b897d67869c72b88f8bb912c09bf33f5de
Alicja <amalinowska.p@gmail.com>
2020-03-13 14:59:37 +0000

    items can now be added by different users

--- shopping_list.py
+++ shopping_list.py
@@ -16,6 +16,16 @@ j_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+DAFAULT_LIST_NAME = 'List'
+
+def list_key(name = DAFAULT_LIST_NAME):
+    return ndb.Key('Shopping List', name)
+
+class List_item(ndb.Model):
+    user_id = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    item = ndb.StringProperty(indexed=False)
+
 class Home(webapp2.RequestHandler):
     def get(self):
         
@@ -25,11 +35,28 @@ class Home(webapp2.RequestHandler):
 class NewList(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        if user:
-            template = j_env.get_template('list.html')
-            self.response.write(template.render())
+        list_query = List_item.query(List_item.user_id == user.user_id(), ancestor = list_key()).order(-List_item.date)
+        list_items = list_query.fetch()
+        url = users.create_logout_url(self.request.uri)
+        linktext = 'Logout'
+        values = {
+            'nickname': user.nickname(),
+            'url': url,
+            'linktext': linktext,
+            'list_items': list_items
+        }
+        template = j_env.get_template('list.html')
+        self.response.write(template.render(values))
+
+class Add(webapp2.RequestHandler):
+    def post(self):
+        user = users.get_current_user()
+        new_item = List_item(user_id = user.user_id(), item = self.request.get('item'), parent = list_key())
+        new_item.put()
+        self.redirect('/list.html')
 
 app = webapp2.WSGIApplication([
     ('/', Home),
-    ('/list.html', NewList)
+    ('/list.html', NewList),
+    ('/list.html/add', Add)
 ], debug=True)
