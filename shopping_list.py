f7c540d08e2f8b0ecd9554041add43e18832f9b7
Alicja <amalinowska.p@gmail.com>
2020-03-21 15:12:48 +0000

    docstrings added; unnecessary code removed(userid from list model)

--- shopping_list.py
+++ shopping_list.py
@@ -21,16 +21,19 @@ def user_key(id):
     '''creates a user key to use as an ancestor for a list'''
     return ndb.Key('User', id)
 
+
 class List(ndb.Model):
-     user_id = ndb.StringProperty()
-     date = ndb.DateTimeProperty(auto_now_add=True)
-     name = ndb.StringProperty()
+    '''Model to represent each list entity'''
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    name = ndb.StringProperty()
 
 class List_item(ndb.Model):
+    '''Model to represent each item entity'''
     date = ndb.DateTimeProperty(auto_now_add=True)
     item = ndb.StringProperty(indexed=False)
 
 class Home(webapp2.RequestHandler):
+    '''Renders index.html for the homepage view'''
     def get(self):
         user = users.get_current_user()
         if user:
@@ -48,12 +51,19 @@ class Home(webapp2.RequestHandler):
         self.response.write(template.render(values))
 
 class Dashboard(webapp2.RequestHandler):
+    '''Handles the main view after login.
+       Gets all the lists and all the items for the current
+       list so they can be used in html to display them. 
+       It also takes an information from URL if the list name
+       that was attempted to add exists, if that is the case
+       it creates a message that this name already exists. 
+    '''
     def get(self):
         user = users.get_current_user()
-        list_query = List.query(List.user_id == user.user_id(), ancestor = user_key(user.user_id())).order(-List.date)
+        list_query = List.query(ancestor = user_key(user.user_id())).order(-List.date)
         lists = list_query.fetch()
         current_list_name = self.request.get('list')
-        list_name_query = List.query(List.user_id == user.user_id(), List.name == current_list_name, ancestor = user_key(user.user_id()))
+        list_name_query = List.query(List.name == current_list_name, ancestor = user_key(user.user_id()))
         current_list = list_name_query.fetch()
         # if a list was selected get its items, otherwise no items should be displayed
         if len(current_list) > 0:
@@ -87,9 +97,12 @@ class Dashboard(webapp2.RequestHandler):
         
 
 class AddList(webapp2.RequestHandler):
+    ''' Adds a list only if a list of this name does not
+    already exists in the database. Sends an information
+    if the name already exists or not on the URL'''
     def post(self):
         user = users.get_current_user()
-        list_query = List.query(List.user_id == user.user_id(), ancestor = user_key(user.user_id())).order(-List.date)
+        list_query = List.query(ancestor = user_key(user.user_id())).order(-List.date)
         lists = list_query.fetch()
         current_list_name = self.request.get('list')
         exists = False
@@ -99,7 +112,7 @@ class AddList(webapp2.RequestHandler):
                 exists = True
                 break
         if not exists:
-            new_list = List(user_id = user.user_id(), name = current_list_name, parent = user_key(user.user_id()))
+            new_list = List(name = current_list_name, parent = user_key(user.user_id()))
             new_list.put()
                
         query_params = {'list': current_list_name, 'exists': exists}
@@ -107,10 +120,12 @@ class AddList(webapp2.RequestHandler):
 
 
 class AddItem(webapp2.RequestHandler):
+    ''' Adds an item to a specifi list. It does not add anything if 
+    a list is not selected '''
     def post(self):
         user = users.get_current_user()
         current_list_name = self.request.get('list_name')
-        list_query = List.query(List.user_id == user.user_id(), List.name == current_list_name, ancestor = user_key(user.user_id()))
+        list_query = List.query(List.name == current_list_name, ancestor = user_key(user.user_id()))
         current_list = list_query.fetch()
         if len(current_list) > 0:
             new_item = List_item(item = self.request.get('item'), parent = current_list[0].key)
@@ -119,10 +134,11 @@ class AddItem(webapp2.RequestHandler):
         self.redirect('/list.html?' + urllib.urlencode(query_params))
             
 class DeleteList(webapp2.RequestHandler):
+    ''' Deletes a currently selected list together with all its items '''
     def post(self):
         user = users.get_current_user()
         current_list_name = self.request.get('list_name')   
-        list_query = List.query(List.user_id == user.user_id(), List.name == current_list_name, ancestor = user_key(user.user_id()))
+        list_query = List.query(List.name == current_list_name, ancestor = user_key(user.user_id()))
         current_list = list_query.fetch()
         if len(current_list) > 0:
             items_query = List_item.query(ancestor = current_list[0].key)
@@ -134,10 +150,11 @@ class DeleteList(webapp2.RequestHandler):
         
 
 class DeleteAll(webapp2.RequestHandler):
+    ''' Deletes all the items from a selected list '''
     def post(self):
         user = users.get_current_user()
         current_list_name = self.request.get('list_name')
-        list_query = List.query(List.user_id == user.user_id(), List.name == current_list_name, ancestor = user_key(user.user_id()))
+        list_query = List.query(List.name == current_list_name, ancestor = user_key(user.user_id()))
         current_list = list_query.fetch()
         if len(current_list) > 0:
             items_query = List_item.query(ancestor = current_list[0].key)
@@ -149,10 +166,13 @@ class DeleteAll(webapp2.RequestHandler):
         
 
 class DeleteItem(webapp2.RequestHandler):
+    ''' Deletes a chosen item from a selected list.
+        This happens by recreating the item key
+        using its ID which was saved as a html element id'''
     def post(self):
         user = users.get_current_user()
         current_list_name = self.request.get('list_name')
-        list_query = List.query(List.user_id == user.user_id(), List.name == current_list_name, ancestor = user_key(user.user_id()))
+        list_query = List.query(List.name == current_list_name, ancestor = user_key(user.user_id()))
         current_list = list_query.fetch()
         item_id = self.request.get('item_id')
         key = ndb.Key('User', str(user.user_id()), 'List', int(current_list[0].key.id()), 'List_item', int(item_id))
@@ -162,10 +182,13 @@ class DeleteItem(webapp2.RequestHandler):
         
 
 class EditItem(webapp2.RequestHandler):
+    ''' Allows to edit an existing item of the selected list.
+        Recreates the item's key and then, using the key,
+        gets the item and edits with the new value of input'''
     def post(self):
         user = users.get_current_user()
         current_list_name = self.request.get('list_name')
-        list_query = List.query(List.user_id == user.user_id(), List.name == current_list_name, ancestor = user_key(user.user_id()))
+        list_query = List.query(List.name == current_list_name, ancestor = user_key(user.user_id()))
         current_list = list_query.fetch()
         item_id = self.request.get('item_id')
         edited_item = self.request.get('edit_item')
@@ -186,5 +209,4 @@ app = webapp2.WSGIApplication([
     ('/list.html/delete', DeleteItem),
     ('/list.html/edit', EditItem),
     ('/list.html/delete-list', DeleteList)
-    
-], debug=True)# remember to remove!!!!!!!!!!!
+])
